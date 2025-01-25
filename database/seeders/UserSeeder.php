a2b43957d1d118bbb8df375c6516a4835739deba
Abraham Hernandez <abrahamycaldera@gmail.com>
2025-01-25 17:28:50 -0400

    RoleSeeder y UserSeeder creados

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Enums\Role;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    $User = User::create([
+      "name" => "Ivana Galeno",
+      "email" => "ivana@gmail.com",
+      "password" => bcrypt("Ivana123."),
+    ])->assignRole(Role::ADMIN);
+  }
+}
