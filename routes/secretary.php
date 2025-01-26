8d62dfee2ac72ca52ab8742549b5dd8ac40231f7
Abraham Hernandez <abrahamycaldera@gmail.com>
2025-01-26 03:18:54 -0400

    Rutas de la app optimizadas

--- routes/secretary.php
+++ routes/secretary.php
@@ -4,27 +4,27 @@ use App\Http\Controllers\SecretariaController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth')->group(function () {
-    // Ruta para la lista de secreatria
-    Route::get('secretaria', [SecretariaController::class, 'index'])
-        ->name('secretaria.index');
+  // Ruta para la lista de secreatria
+  Route::get('secretaria', [SecretariaController::class, 'index'])
+    ->name('secretaria.index');
 
-    // Ruta para almacenar un nuevo secretaria
-    Route::post('secretaria', [SecretariaController::class, 'store'])
-        ->name('secretaria.store');
+  // Ruta para almacenar un nuevo secretaria
+  Route::post('secretaria', [SecretariaController::class, 'store'])
+    ->name('secretaria.store');
 
-    // Ruta para obtener una secretaria específico en formato JSON
-    Route::get('secretaria/{id}', [SecretariaController::class, 'show'])
-        ->name('secretaria.show');
+  // Ruta para obtener una secretaria específico en formato JSON
+  Route::get('secretaria/{id}', [SecretariaController::class, 'show'])
+    ->name('secretaria.show');
 
-    // Ruta para eliminar un secretaria
-    Route::delete('secretaria/{id}', [SecretariaController::class, 'destroy'])
-        ->name('secretaria.destroy');
+  // Ruta para eliminar un secretaria
+  Route::delete('secretaria/{id}', [SecretariaController::class, 'destroy'])
+    ->name('secretaria.destroy');
 
-    // Ruta para mostrar el formulario de edición de un secretaria
-    Route::get('secretaria/{id}/edit', [SecretariaController::class, 'edit'])
-        ->name('secretaria.edit');
+  // Ruta para mostrar el formulario de edición de un secretaria
+  Route::get('secretaria/{id}/edit', [SecretariaController::class, 'edit'])
+    ->name('secretaria.edit');
 
-    // Ruta para actualizar un secretaria
-    Route::put('secretaria/{id}', [SecretariaController::class, 'update'])
-        ->name('secretaria.update');
+  // Ruta para actualizar un secretaria
+  Route::put('secretaria/{id}', [SecretariaController::class, 'update'])
+    ->name('secretaria.update');
 });
--- routes/specialist.php
+++ routes/specialist.php
@@ -4,32 +4,31 @@ use App\Http\Controllers\EspecialistaController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth')->group(function () {
+  // Ruta para la lista de especialistas
+  Route::get('especialista', [EspecialistaController::class, 'index'])
+    ->name('especialista.index');
 
-    // Ruta para la lista de especialistas
-    Route::get('especialista', [EspecialistaController::class, 'index'])
-        ->name('especialista.index');
+  // Ruta para obtener un especialista específico en formato JSON
+  Route::get('especialista/{id}', [EspecialistaController::class, 'show'])
+    ->name('especialista.show');
 
-    // Ruta para obtener un especialista específico en formato JSON
-    Route::get('especialista/{id}', [EspecialistaController::class, 'show'])
-        ->name('especialista.show');
+  // Ruta para almacenar un nuevo especialista
+  Route::post('especialista', [EspecialistaController::class, 'store'])
+    ->name('especialista.store');
 
-    // Ruta para almacenar un nuevo especialista
-    Route::post('especialista', [EspecialistaController::class, 'store'])
-        ->name('especialista.store');
+  // Ruta para mostrar un especialista específico
+  Route::get('especialista/{id}', [EspecialistaController::class, 'show'])
+    ->name('especialista.show');
 
-    // Ruta para mostrar un especialista específico
-    Route::get('especialista/{id}', [EspecialistaController::class, 'show'])
-        ->name('especialista.show');
+  // Ruta para mostrar el formulario de edición de un especialista
+  Route::get('especialista/{id}/edit', [EspecialistaController::class, 'edit'])
+    ->name('especialista.edit');
 
-    // Ruta para mostrar el formulario de edición de un especialista
-    Route::get('especialista/{id}/edit', [EspecialistaController::class, 'edit'])
-        ->name('especialista.edit');
+  // Ruta para actualizar un especialista específico
+  Route::put('especialista/{id}', [EspecialistaController::class, 'update'])
+    ->name('especialista.update');
 
-    // Ruta para actualizar un especialista específico
-    Route::put('especialista/{id}', [EspecialistaController::class, 'update'])
-        ->name('especialista.update');
-
-    // Ruta para eliminar un especialista específico
-    Route::delete('especialista/{id}', [EspecialistaController::class, 'destroy'])
-        ->name('especialista.destroy');
+  // Ruta para eliminar un especialista específico
+  Route::delete('especialista/{id}', [EspecialistaController::class, 'destroy'])
+    ->name('especialista.destroy');
 });
