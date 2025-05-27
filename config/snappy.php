acf6e4e05cfeeee61c95f23e8595796df62129c5
Abraham Hernandez <abrahamycaldera@gmail.com>
2025-05-26 21:45:59 -0400

    .env.example file, updated

--- config/snappy.php
+++ config/snappy.php
@@ -1,8 +1,7 @@
 <?php
 
 return [
-
-    /*
+  /*
     |--------------------------------------------------------------------------
     | Snappy PDF / Image Configuration
     |--------------------------------------------------------------------------
@@ -11,20 +10,19 @@ return [
     |
     */
 
-    'pdf' => [
-        'enabled' => true,
-        'binary'  => env('WKHTML_PDF_BINARY', '/usr/bin/wkhtmltopdf'),
-        'timeout' => false,
-        'options' => [],
-        'env'     => [],
-    ],
-    
-    'image' => [
-        'enabled' => true,
-        'binary'  => env('WKHTML_IMG_BINARY', '/usr/bin/wkhtmltoimage'),
-        'timeout' => false,
-        'options' => [],
-        'env'     => [],
-    ],
+  'pdf' => [
+    'enabled' => true,
+    'binary'  => env('WKHTML_PDF_BINARY', '/usr/bin/wkhtmltopdf'),
+    'timeout' => false,
+    'options' => [],
+    'env'     => [],
+  ],
 
+  'image' => [
+    'enabled' => true,
+    'binary'  => env('WKHTML_IMG_BINARY', '/usr/bin/wkhtmltoimage'),
+    'timeout' => false,
+    'options' => [],
+    'env'     => [],
+  ],
 ];
