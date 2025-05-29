7a05059e3ea8d148f29b93716e6dac866d46bc14
Abraham Hernandez <abrahamycaldera@gmail.com>
2025-05-29 18:38:40 -0400

    navbar component, created

---
+++ resources/views/components/navbar/index.blade.php
@@ -0,0 +1,25 @@
+<nav class="full-box dashboard-Navbar">
+  <ul class="full-box list-unstyled text-right">
+    <li class="pull-left">
+      <a href="#!" class="btn-menu-dashboard"><i class="zmdi zmdi-more-vert"></i></a>
+    </li>
+
+    <li>
+      <a href="#!" class="btn-Notifications-area">
+        <i class="zmdi zmdi-notifications-none"></i>
+        <span class="badge">7</span>
+      </a>
+    </li>
+
+    <li>
+      <a href="#!" class="btn-dropdown">
+        <i class="zmdi zmdi-account"></i>
+      </a>
+    </li>
+    <li>
+      <a href="#!" class="btn-ayuda-interactiva" onclick="iniciarAyuda()">
+        <i class="zmdi zmdi-help-outline"></i>
+      </a>
+    </li>
+  </ul>
+</nav>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,31 +10,7 @@
 
   <main class="full-box dashboard-contentPage">
     <!-- NavBar -->
-    <nav class="full-box dashboard-Navbar">
-      <ul class="full-box list-unstyled text-right">
-        <li class="pull-left">
-          <a href="#!" class="btn-menu-dashboard"><i class="zmdi zmdi-more-vert"></i></a>
-        </li>
-
-        <li>
-          <a href="#!" class="btn-Notifications-area">
-            <i class="zmdi zmdi-notifications-none"></i>
-            <span class="badge">7</span>
-          </a>
-        </li>
-
-        <li>
-          <a href="#!" class="btn-dropdown">
-            <i class="zmdi zmdi-account"></i>
-          </a>
-        </li>
-        <li>
-          <a href="#!" class="btn-ayuda-interactiva" onclick="iniciarAyuda()">
-            <i class="zmdi zmdi-help-outline"></i>
-          </a>
-        </li>
-      </ul>
-    </nav>
+    <x-navbar />
 
     <!-- Content page -->
 
